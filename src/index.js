4d8b8d69f70f46ebe33ebbfac046d2a8538b0d7e
leon <de.nizard@hotmail.com>
2023-04-17 12:39:27 -0300

    Reestruturado a página da Web Movie-API usando o conceito de Kanban para melhor gerenciamento de tarefas. Comecei a partir do desenvolvimento móvel e reestruturei o CSS para melhorar a aparência em diferentes dispositivos. Adicionei uma pasta chamada 'Wireframes' para armazenar todas as versões do esboço da interface do usuário.

--- src/index.js
+++ src/index.js
@@ -3,58 +3,73 @@ const body = document.querySelector("body"),
   imgCenter = document.querySelector(".wrapper-player > img");
 
 for (const iterator of imgWrapper) {
-
   const img = iterator.querySelector("img");
 
   iterator.addEventListener("click", () => {
-
     body.style.backgroundImage = `url(${img.getAttribute("src")})`;
     imgCenter.setAttribute("src", img.getAttribute("src"));
-    
   });
 }
 
 //close nav
 
-const btnClose = document.querySelector('#close')
-const nav = document.querySelector('nav')
-btnClose.addEventListener('click', ()=>{
-  nav.classList.toggle('closed')
+const btnClose = document.querySelector("#close");
+const nav = document.querySelector("nav");
+btnClose.addEventListener("click", () => {
+  nav.classList.toggle("closed");
 
-  if(nav.classList.contains('closed')){
-    
-    btnClose.classList.remove('ph-x')
-    btnClose.classList.add('ph-list')
-
-  }else{
-    btnClose.classList.remove('ph-list')
-    btnClose.classList.add('ph-x')
+  if (nav.classList.contains("closed")) {
+    btnClose.classList.remove("ph-x");
+    btnClose.classList.add("ph-list");
+  } else {
+    btnClose.classList.remove("ph-list");
+    btnClose.classList.add("ph-x");
   }
-
-})
-
+});
 
 //indicator page list
 
-const listPage = document.querySelectorAll('.header-start ul li a')
+const listPage = document.querySelectorAll(".header-start ul li a");
 
-function handleClick(event){
-
+function handleClick(event) {
   //first remove class menu
-  listPage.forEach(a =>{
-    a.classList.remove('menu')
-  })
+  listPage.forEach((a) => {
+    a.classList.remove("menu");
+  });
 
   //add class menu in element clicked
-  event.currentTarget.classList.add('menu')
+  event.currentTarget.classList.add("menu");
 }
 
-listPage.forEach(a =>{
-  a.addEventListener('click', handleClick)
+listPage.forEach((a) => {
+  a.addEventListener("click", handleClick);
 
-  const icon = a.querySelector('i')
-  if(icon){
-    icon.addEventListener('click', handleClick)
+  const icon = a.querySelector("i");
+  if (icon) {
+    icon.addEventListener("click", handleClick);
   }
-})
+});
 
+function loadSwipper() {
+  var swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop: true,
+
+    navigation: {
+      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+    },
+  });
+}
+
+window.addEventListener("load", loadSwipper);
