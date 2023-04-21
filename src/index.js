e9e4e6783947dc698aef24a8a154d7e9c663a753
leon <de.nizard@hotmail.com>
2023-04-20 21:45:22 -0300

    Adicionada função loadMovies() que carrega os filmes através da API, cria os slides do swiper com as imagens de poster e inicia o swiper. Também foi criada a função atualizaBackground() que atualiza o background com base na imagem de banner do filme selecionado no swiper. Por fim, a função loadSwiper() foi atualizada para receber a função atualizaBackground() como callback e assim atualizar o background com a imagem de banner correta.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 const btnmenu = document.querySelector('.container-icon')
 const header = document.querySelector('header')
-const swiper = document.querySelector('.swiper');
+const swiper = document.querySelector('.swiper')
+const swiperWrapper = document.querySelector('.swiper-wrapper')
 
 
 
-
+//Função responsavél por abrir e fechar o menu do navbar
 function toggleMenu(){
   header.classList.toggle('active')
 }
 btnmenu.addEventListener('click', toggleMenu)
 
+// Função global atualizaBackground
+function atualizaBackground(movies, index){
+  const movie = movies[index]
 
-function loadSwiper(){
+  console.log(movie.backdrop_path)
+  const imgBackground = document.querySelector('#background');
+  if (imgBackground) {
+    imgBackground.src = `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`;
+  }
+}
+
+//Função responsável por iniciar o Swiper
+function loadSwiper(movies){
   const swiper = new Swiper('.swiper', {
     effect: "cards",
     cardsEffect:{
@@ -20,8 +32,57 @@ function loadSwiper(){
       slideShadows: true,
     },
     loop: true,
+    on:{
+      slideChange: function(){
+        atualizaBackground(movies, this.activeIndex)
+      },
+    },
   });
 }
 
+const key = 'fd298ef799ed7bc469fd73887cdfcc2e'
+const api = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=pt-BR&sort_by=popularity.desc`
 
-window.addEventListener('load', loadSwiper)
+async function loadMovies(){
+
+  try {
+    const response = await fetch(api)
+    const data = await response.json()
+    const movies = data.results
+
+    // Criando os slides do Swiper
+    movies.forEach(movie => {
+      criaSwiperSlideImg(`https://image.tmdb.org/t/p/w500${movie.poster_path}`)
+    })  
+
+    // Iniciando o Swiper e atualizando o background com o primeiro filme
+    loadSwiper(movies)
+    atualizaBackground(movies, 0)
+
+  } catch (error) {
+    console.log(error)
+  }
+  
+}
+
+loadMovies()
+
+
+
+
+function criaSwiperSlideImg(src){
+
+  const swiperSlide = document.createElement('div')
+  swiperSlide.setAttribute('class', 'swiper-slide')
+
+  swiperWrapper.appendChild(swiperSlide)
+
+  const img = document.createElement('img')
+  img.setAttribute('src', src)
+
+  swiperSlide.appendChild(img)
+}
+
+
+
+
