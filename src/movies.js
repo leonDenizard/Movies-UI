38c737285b59704a1605bdbb0751b5638c826bbb
leon <de.nizard@hotmail.com>
2023-05-02 21:17:13 -0300

    Criado função responsável por criar os slides do swiper

--- src/movies.js
+++ src/movies.js
@@ -1,33 +1,71 @@
-const btnMenu = document.querySelector('.container-icon')
-const headerMenu = document.querySelector('.container-menu')
+const btnMenu = document.querySelector(".container-icon");
+const headerMenu = document.querySelector(".container-menu");
+const swiper = document.querySelector('.mySwiper')
 
-function toggleMenu(){
-    btnMenu.classList.toggle('active')
-    headerMenu.classList.toggle('active')
-
+function toggleMenu() {
+  btnMenu.classList.toggle("active");
+  headerMenu.classList.toggle("active");
 }
-btnMenu.addEventListener('click', toggleMenu)
+btnMenu.addEventListener("click", toggleMenu);
 
-const listLinks = document.querySelectorAll('.container-menu > ul > li')
+const listLinks = document.querySelectorAll(".container-menu > ul > li");
 
-listLinks.forEach(link => {
-    link.addEventListener('click', ()=>{
-        listLinks.forEach(efeito => efeito.classList.remove('active'))
+listLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    listLinks.forEach((efeito) => efeito.classList.remove("active"));
 
-        link.classList.add('active')
-    })
-})
-listLinks[0].classList.add('active')
+    link.classList.add("active");
+  });
+});
+listLinks[0].classList.add("active");
 
-function loadSwiper(){
-    swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        centeredSlides: false,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      });
+function loadSwiper() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 }
-loadSwiper()
+// loadSwiper();
+
+function criaSwiperSlideImg(src, titulo) {
+    //Cria swiper-slide
+    const swiperSlide = document.createElement('swiper-slide')
+    
+    //cria a div com a classe wrapper-slide
+    const divWrapperSlide = document.createElement('div')
+    divWrapperSlide.classList.add('wrapper-slide')
+
+    //cria img e atribui o src do parametro
+    const img = document.createElement('img')
+    img.src = src
+
+    //cria o icone e adiciona a classe do phosphoricon
+    const i = document.createElement('i')
+    i.classList.add('fa-sharp')
+    i.classList.add('fa-solid')
+    i.classList.add('fa-heart')
+
+    //cria o titulo do filme e atualiza com titulo do parametro
+    const tituloH3 = document.createElement('h3')
+    tituloH3.textContent = titulo
+
+    //Adiciona swiper-slide dentro do swiper-container
+    swiper.appendChild(swiperSlide)
+    //Adiciona div wrapper-slide dentro do elemento swiper-slide
+    swiperSlide.appendChild(divWrapperSlide)
+    //Adiciona img dentro da divWrapperSlide
+    divWrapperSlide.appendChild(img)
+    //Adiciona o icone dentro da divWrapperSlide
+    divWrapperSlide.appendChild(i)
+    //Adiciona titulo no divWrapperSlide
+    divWrapperSlide.appendChild(tituloH3)
+
+    console.log('funciono')
+}
+
+criaSwiperSlideImg('./assets/guardians.jpg', 'Guardioes')
