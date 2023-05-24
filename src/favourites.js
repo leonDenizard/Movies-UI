ed39f2b49ed9fb50c03f77dec15c6561996e7b4c
leon <de.nizard@hotmail.com>
2023-05-24 14:17:44 -0300

    Criado favourites.js e iniciado manipulação dos dados, recuperados os ids do localStorage e criado função que armazena os filmes da api, implementado função que cria o grid de favoritos com base nos filmes favoritados

---
+++ src/favourites.js
@@ -0,0 +1,78 @@
+const favourites = localStorage.getItem("movieIds")
+const cleanFavourites = favourites.slice(1, -1)
+const ids = cleanFavourites.split(",");
+
+const key = "fd298ef799ed7bc469fd73887cdfcc2e";
+
+
+function createGrid(src, imdb, title){
+
+    //Ponto onde será inserido os elementos
+    const containerFavourite = document.querySelector('.container-favourite')
+
+    const wrapperMovie = document.createElement('div')
+    wrapperMovie.classList.add('wrapper-movie')
+
+    containerFavourite.appendChild(wrapperMovie)
+
+    //criando a imagem que recebera o src da API
+    const imgMovie = document.createElement('img')
+    imgMovie.src = `https://image.tmdb.org/t/p/w500${src}`
+    wrapperMovie.appendChild(imgMovie)
+
+    // Criado o icone do coração e adicionando as classe e inserindo dentro de wrapper-movie
+    const i = document.createElement('i')
+    i.classList.add('fa-sharp')
+    i.classList.add('fa-solid')
+    i.classList.add('fa-heart')
+    wrapperMovie.appendChild(i)
+
+    const divDetail = document.createElement('div')
+    divDetail.classList.add('detail')
+    wrapperMovie.appendChild(divDetail)
+
+    const divImdb = document.createElement('div')
+    divImdb.classList.add('imdb')
+    divDetail.appendChild(divImdb)
+
+    const pImdb = document.createElement('p')
+    pImdb.textContent = imdb.toFixed(2)
+    divImdb.appendChild(pImdb)
+
+    const divTitleMovie = document.createElement('div')
+    divTitleMovie.classList.add('title-movie')
+    divDetail.appendChild(divTitleMovie)
+
+    const pTitle = document.createElement('p')
+    pTitle.textContent = title
+    divTitleMovie.appendChild(pTitle)
+
+}
+
+async function loadFavourites() {
+  try {
+
+    const movies = []
+
+    for(id of ids){
+       const api =  `https://api.themoviedb.org/3/movie/${id}?api_key=${key}&language=pt-BR`
+       const response = await fetch(api)
+       const movie = await response.json()
+
+       movies.push(movie)
+    }
+
+
+    console.log(movies)
+
+    movies.forEach(movie =>{
+        createGrid(movie.poster_path, movie.vote_average, movie.title)
+    })
+
+
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+loadFavourites();
